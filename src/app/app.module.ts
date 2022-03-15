72d1e01f0e2ca5682a5b6418943adbea080be921
ons mejri <onsmejri529@gmail.com>
2022-03-15 14:54:04 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PromotionsComponent } from './promotions/promotions.component';
+import { JeuxComponent } from './jeux/jeux.component';
+import { ReclamationComponent } from './reclamation/reclamation.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PromotionsComponent,
+    JeuxComponent,
+    ReclamationComponent,
+    FeedbackComponent
   ],
   imports: [
     BrowserModule,
